21dcdcbe2d6d9a2809f27e6ae7e60d2de8eeaf21
William Ukoh <william@williamukoh.com>
2019-06-09 18:21:17 +0100

    Project setup modifications. Created `local` folder for testing locally. `dist` for deploying to production

--- dist/index.php
+++ dist/index.php
@@ -1,10 +1,26 @@
-<!-- index.nunjucks -->
-{% set body_class = "home" %}
-{% set page_title = "William Ukoh - Product and Designer based in Lagos, Nigeria" %}
+<!doctype html>
+<html class="no-js" lang="en-NG">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>William Ukoh - Product and Designer based in Lagos, Nigeria</title>
+        <meta name="description" content="I’m a Product Designer and Developer working at the crossroads of design, technology and business.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-{% extends "layout.nunjucks" %}
+        
+        <link href="//fonts.googleapis.com/css?family=Nunito:300,400,500,700" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/build.min.css?t=1560100622730">
+        
+    </head>
+    <body class="home">
 
-{% block content %}
+        <div class="page-wrapper">
+
+            <a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+
+            
+
+            
     
     <div class="main-wrapper">
 
@@ -212,4 +228,26 @@
 
     </div>
 
-{% endblock %}
+
+
+            <div class="footer-wrapper">
+
+    <footer class="text--sm">
+        Design and 
+        <svg height="20" class="fill-gray" aria-label="code" viewBox="0 0 14 16" width="17" role="img">
+            <path fill-rule="evenodd" d="M9.5 3L8 4.5 11.5 8 8 11.5 9.5 13 14 8 9.5 3zm-5 0L0 8l4.5 5L6 11.5 2.5 8 6 4.5 4.5 3z"></path>
+        </svg>
+        with
+        <svg height="20" class="fill-gray" aria-label="love" viewBox="0 0 12 16" version="1.1" width="15" role="img">
+            <path fill-rule="evenodd" d="M11.2 3c-.52-.63-1.25-.95-2.2-1-.97 0-1.69.42-2.2 1-.51.58-.78.92-.8 1-.02-.08-.28-.42-.8-1-.52-.58-1.17-1-2.2-1-.95.05-1.69.38-2.2 1-.52.61-.78 1.28-.8 2 0 .52.09 1.52.67 2.67C1.25 8.82 3.01 10.61 6 13c2.98-2.39 4.77-4.17 5.34-5.33C11.91 6.51 12 5.5 12 5c-.02-.72-.28-1.39-.8-2.02V3z"></path>
+        </svg>
+        by <a href="https://twitter.com/williamukoh">@williamukoh</a>. Unless otherwise noted, all code is free to use under the <a href="https://opensource.org/licenses/MIT">MIT License</a>.
+    </footer>
+
+</div>
+
+        </div>
+
+        <script src="assets/js/build.min.js?t=1560100622730"></script> 
+    </body>
+</html>
